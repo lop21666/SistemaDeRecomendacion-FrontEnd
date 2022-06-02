9580af0384fe126ce7ede535005a3de9f0571ecc
carrillo21108 <87512111+carrillo21108@users.noreply.github.com>
2022-06-02 16:46:46 -0600

    Cambios Recomendación por Usuarios

--- src/app/services/services/user.service.ts
+++ src/app/services/services/user.service.ts
@@ -7,7 +7,7 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class UserService {
-  url = 'http://localhost:3000';
+  url = 'http://192.168.1.20:3000';
   data = null;
   datosUsuario;
 
